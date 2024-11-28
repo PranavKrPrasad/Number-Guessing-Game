8974bcc76cd022e5112d71703e095279e8a8f5c5
PranavKrPrasad <pranav2020pranav@gmail.com>
2024-11-28 22:39:05 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,19 @@
+import random
+
+def number_guessing_game():
+    print("Welcome to the Number Guessing Game!")
+    number_to_guess = random.randint(1, 100)
+    attempts = 0
+    
+    while True:
+        user_guess = int(input("Guess a number between 1 and 100: "))
+        attempts += 1
+        if user_guess < number_to_guess:
+            print("Too low! Try again.")
+        elif user_guess > number_to_guess:
+            print("Too high! Try again.")
+        else:
+            print(f"Congratulations! You guessed the number in {attempts} attempts.")
+            break
+
+number_guessing_game()
